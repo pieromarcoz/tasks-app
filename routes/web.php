35ed5f3283eba17cec45fcebab9406dac9aa45e7
pieromarcoz <75951868+pieromarcoz@users.noreply.github.com>
2024-10-29 16:32:32 -0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/dashboard', function () {
+Route::get('/', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
